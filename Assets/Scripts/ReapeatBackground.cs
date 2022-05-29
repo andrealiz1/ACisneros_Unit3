88283f602adc952dd92047f23a320a8e414bed69
andrealiz1 <99044030+andrealiz1@users.noreply.github.com>
2022-05-29 03:17:43 -0700

    ACisneros_Unit3LessonB
    ACisneros_Unit3LessonB

---
+++ Assets/Scripts/ReapeatBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReapeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float reapeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        reapeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startPos.x - reapeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+}
